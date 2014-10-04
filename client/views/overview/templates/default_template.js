52015d8d10d38358adc13493fb9198635210298e
Leo Bergnéhr <leo@bergnehr.se>
2014-10-04 10:54:25 +0200

    Make the grid layout of the overview more dynamic
    Make the numer of rows and columns customizable with two `Session`
    variables.

--- client/views/overview/templates/default_template.js
+++ client/views/overview/templates/default_template.js
@@ -1,8 +1,22 @@
+Session.setDefault("maxOverviewColumns", 3);
+Session.setDefault("rowsPerOverviewPage", 2);
+
 Template.defaultOverview.helpers({
-  taskGroup: function() {
-    return {
-      firstThree: _.first(this.tasks, 3),
-      threeMore: _.rest(this.tasks, 3)
-    };
+  taskRows: function() {
+    var maxColumns = Session.get("maxOverviewColumns");
+    return _.chain(this.tasks)
+      .groupBy(function(item, index) {
+        return Math.floor(index / maxColumns);
+      })
+      .values()
+      .value();
+  },
+
+  maxOverviewColumns: function() {
+    return Session.get("maxOverviewColumns");
+  },
+
+  rowHeightPercentage: function() {
+    return 100 / Session.get("rowsPerOverviewPage");
   }
 });
