c2c50c8d1a72e23fd5ce261e71fad4794ac860de
Leo Bergnéhr <leo@bergnehr.se>
2014-10-04 12:08:37 +0200

    Make the text in the tiles dynamic
    Add a fancy function wich somewhat dynamically scales the font size in
    the tiles depending on the number of words in the title as well as the
    longest of those words.

--- client/views/overview/templates/default_template.js
+++ client/views/overview/templates/default_template.js
@@ -20,3 +20,23 @@ Template.defaultOverview.helpers({
     return 100 / Session.get("rowsPerOverviewPage");
   }
 });
+
+Template.task.rendered = function() {
+  var taskDiv = this.find(".task");
+  var height = taskDiv.clientHeight;
+  var width = taskDiv.clientWidth;
+
+  var data = this.data;
+
+  var words = data.name.split(" ");
+  var longestWord = _.chain(words)
+    .pluck("length")
+    .max()
+    .value();
+  var numberOfWords = words.length;
+  var squarePixels = height * width;
+
+  var fontSize = 2 / Math.log(longestWord * numberOfWords * numberOfWords) * Math.pow((squarePixels / 1000), 0.6);
+
+  taskDiv.style.fontSize = fontSize + "vw";
+};
